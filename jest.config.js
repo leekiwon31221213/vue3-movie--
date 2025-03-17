5cbe61dc7082e9104eb4037ed643bae4dd6dc8dc
Lee Ki Won <cute01235@nate.com>
2025-03-17 10:22:44 +0900

    [update]

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  moduleFileExtensions: ["js", "vue"],
+
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/src/$1",
+  },
+
+  modulePathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/dist", "<rootDir>/cypress"],
+
+  testURL: "http://localhost/",
+
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    "^.+\\.js$": "babel-jest",
+  },
+};
